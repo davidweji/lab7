b18567c4603735f221dccf987980222d073078b2
davidweji <130021498+davidweji@users.noreply.github.com>
2025-03-21 02:21:12 -0300

    debugging new errors

--- netlify/functions/projectsAPI.js
+++ netlify/functions/projectsAPI.js
@@ -6,9 +6,14 @@ import fs from "fs";
 dotenv.config();
 const api = express();
 const router = express.Router();
+let projectData = [];
 
-const projectData = JSON.parse(fs.readFileSync("./netlify/functions/db.json"));
-
+try{
+    projectData = JSON.parse(fs.readFileSync(`${__dirname}/db.json`));
+}
+catch (err) {
+    console.log("failed to read db.json: ", err);
+}
 router.get("/", (req, res) => {
     if(!projectData || projectData.length === 0) {
         return res.status(404).json({error: "No projects found."});
